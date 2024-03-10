a8e5d9fc8b37a55e15d603a75f1fd0994a178da7
Raider-yt <85959905+Raider-yt@users.noreply.github.com>
2024-03-10 15:43:51 +0000

    Add files via upload

---
+++ 1v1.lol/js/unityUrls.js
@@ -0,0 +1,9 @@
+window.loadedUrls = true;
+
+function getBuildUrl() {
+	return "https://justbuild.nyc3.cdn.digitaloceanspaces.com/NewCI/1v1/Prod/243/cf11d3027abc49888fc24d53ba6e589f";
+}
+
+function getGameLoaderUrl() {
+	return getBuildUrl() + "/WebGL.loader.js";
+}
